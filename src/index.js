27923d61e8bf11a838e2580176c7cecd9f6b4b8d
Lachezar Dimitrov <lachezar.dimitrov@Lachezars-Mac-mini.local>
2020-11-26 16:06:42 +0200

    Webpack config, rebuilding and restarting.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
+const React = require('react');
+
+const { renderToString } = require('react-dom/server');
+
+const Home = require('./client/components/Home').default;
+
 const app = express();
 
-app.get('/', (request, response) => { });
+app.get('/', (request, response) => {
+  const content = renderToString(<Home />);
+
+  response.send(content);
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
